7b2de0dd7cfb085486a0cb3fe03b7c6f5652ec89
JyotimoyKashyap <jyotimoykashyap123@gmail.com>
2021-05-08 19:19:35 +0530

    Intro Fragment setup completed and Camera permissions taken from the user

---
+++ app/src/main/java/com/jyotimoykashyap/runanddetect/Camera.kt
@@ -0,0 +1,11 @@
+package com.jyotimoykashyap.runanddetect
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Camera : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_camera)
+    }
+}
--- app/src/main/java/com/jyotimoykashyap/runanddetect/IntroFragment/IntroFragment.kt
+++ app/src/main/java/com/jyotimoykashyap/runanddetect/IntroFragment/IntroFragment.kt
@@ -5,15 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
 import com.jyotimoykashyap.runanddetect.R
 import com.jyotimoykashyap.runanddetect.databinding.FragmentIntroBinding
+import pub.devrel.easypermissions.AppSettingsDialog
+import pub.devrel.easypermissions.EasyPermissions
 
 
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
 
-class IntroFragment : Fragment() {
+class IntroFragment : Fragment() , EasyPermissions.PermissionCallbacks{
 
     private var param1: String? = null
     private var param2: String? = null
@@ -37,6 +41,24 @@ class IntroFragment : Fragment() {
         _binding = FragmentIntroBinding.inflate(inflater, container, false)
         // your code goes here
 
+        binding.openCameraBtn.setOnClickListener{
+            if(!hasCameraPermission()) requestCameraPermission()
+            else{
+                // showing a toast message
+                Toast.makeText(
+                    context,
+                    "Camera Permission Given",
+                    Toast.LENGTH_SHORT
+                ).show()
+
+                /**
+                 * Opening the camera activity
+                 */
+                findNavController().navigate(R.id.action_introFragment_to_camera)
+
+            }
+        }
+
 
 
 
@@ -48,7 +70,50 @@ class IntroFragment : Fragment() {
         _binding = null
     }
 
+    private fun hasCameraPermission() =
+        EasyPermissions.hasPermissions(
+            requireContext(),
+            android.Manifest.permission.CAMERA
+        )
+
+    private fun requestCameraPermission(){
+        EasyPermissions.requestPermissions(
+            this,
+            "Camera will be used by this application to detect your running pose",
+            PERMISSION_REQUEST_FOR_CAMERA,
+            android.Manifest.permission.CAMERA
+
+        )
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this)
+    }
+
+    override fun onPermissionsGranted(requestCode: Int, perms: MutableList<String>) {
+        Toast.makeText(
+            requireContext(),
+            "Permission Granted",
+            Toast.LENGTH_SHORT
+        ).show()
+    }
+
+    override fun onPermissionsDenied(requestCode: Int, perms: MutableList<String>) {
+        if(EasyPermissions.somePermissionPermanentlyDenied(this, perms)){
+            AppSettingsDialog.Builder(requireActivity()).build().show()
+        }else{
+            requestCameraPermission()
+        }
+    }
+
     companion object {
+
+        const val PERMISSION_REQUEST_FOR_CAMERA = 1
 
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
