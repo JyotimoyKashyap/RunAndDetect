91e9ca7945e8bbf979c44aeb4cf7d57312faa59c
JyotimoyKashyap <jyotimoykashyap123@gmail.com>
2021-05-09 20:46:44 +0530

    Sign out functionality set up complete

--- app/src/main/java/com/jyotimoykashyap/runanddetect/IntroFragment/IntroFragment.kt
+++ app/src/main/java/com/jyotimoykashyap/runanddetect/IntroFragment/IntroFragment.kt
@@ -7,6 +7,12 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.shape.CornerFamily
+import com.google.android.material.shape.MaterialShapeDrawable
+import com.google.android.material.textview.MaterialTextView
+import com.google.firebase.auth.FirebaseAuth
 import com.jyotimoykashyap.runanddetect.R
 import com.jyotimoykashyap.runanddetect.databinding.FragmentIntroBinding
 import pub.devrel.easypermissions.AppSettingsDialog
@@ -25,6 +31,10 @@ class IntroFragment : Fragment() , EasyPermissions.PermissionCallbacks{
     private var _binding : FragmentIntroBinding? = null
     private val binding get() = _binding!!
 
+    private lateinit var materialAlertDialogBuilder: MaterialAlertDialogBuilder
+    private lateinit var customAlertDialogView : View
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -40,6 +50,18 @@ class IntroFragment : Fragment() , EasyPermissions.PermissionCallbacks{
         // Inflate the layout for this fragment
         _binding = FragmentIntroBinding.inflate(inflater, container, false)
         // your code goes here
+        auth = FirebaseAuth.getInstance()
+
+        // material dialog
+        materialAlertDialogBuilder = MaterialAlertDialogBuilder(requireContext())
+
+        // showing the dialog
+        binding.accountIcon.setOnClickListener{
+            customAlertDialogView = LayoutInflater.from(context)
+                .inflate(R.layout.account_dialog_layout, null, false)
+
+            openDialog()
+        }
 
         binding.openCameraBtn.setOnClickListener{
             if(!hasCameraPermission()) requestCameraPermission()
@@ -63,6 +85,33 @@ class IntroFragment : Fragment() , EasyPermissions.PermissionCallbacks{
 
 
         return binding.root
+    }
+
+    private fun openDialog() {
+        materialAlertDialogBuilder.setView(customAlertDialogView)
+
+        val background = materialAlertDialogBuilder.background as MaterialShapeDrawable
+
+        background.shapeAppearanceModel =
+            background.shapeAppearanceModel.toBuilder()
+                .setAllCorners(CornerFamily.ROUNDED, 24.toFloat())
+                .build()
+
+        val alertDialog = materialAlertDialogBuilder.show()
+
+
+        val emailText = customAlertDialogView.findViewById<MaterialTextView>(R.id.email_address_text_view)
+        val signOutBtn = customAlertDialogView.findViewById<MaterialButton>(R.id.sign_out_btn)
+
+        emailText.text = auth.currentUser.email.toString()
+
+        signOutBtn.setOnClickListener{
+            auth.signOut()
+            findNavController().navigate(R.id.action_introFragment_to_loginFragment)
+            alertDialog.dismiss()
+        }
+
+
     }
 
     override fun onDestroyView() {
